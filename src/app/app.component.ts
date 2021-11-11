72fb3bbf3dfc6f6b00ec4ecca60053e917c4cf8c
AlinCristian97 <alin.cristian977@gmail.com>
2021-11-11 10:55:50 +0200

    Prepared project for new section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub: Subscription;
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    })
-  }
-
-  ngOnDestroy() {
-    this.activatedSub.unsubscribe();
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
